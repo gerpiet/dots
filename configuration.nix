d2a7576a1a834d31b949d4f8ddca0902c767decf
Pieter Gerets <pieter.gerets@gmail.com>
2024-03-20 15:17:20 +0100

    Update

--- configuration.nix
+++ configuration.nix
@@ -175,6 +175,9 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    gdb
+    lua
+    love
     php
     cargo
     clippy
