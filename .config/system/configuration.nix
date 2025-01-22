c9010bc8348f2d480aec9d12e0a734db9c7e4e37
Pieter Gerets <pieter.gerets@gmail.com>
2025-01-22 17:23:57 +0100

    Further remove unnecessary comments

--- .config/system/configuration.nix
+++ .config/system/configuration.nix
@@ -59,10 +59,6 @@
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # ^^^ Enabled by Gnome ^^^
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Enable networking
   networking.networkmanager.enable = true;
 
@@ -99,12 +95,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -178,11 +168,6 @@
 
   services.xserver.excludePackages = with pkgs; [
     xterm
-  ];
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
