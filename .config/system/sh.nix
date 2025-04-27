272514b26992506ee265022aa89bea95657979c9
Pieter Gerets <pieter.gerets@gmail.com>
2025-04-27 17:09:54 +0200

    Bend xdg-compliance

--- .config/system/sh.nix
+++ .config/system/sh.nix
@@ -17,4 +17,21 @@ in
   programs.bash = {
     shellAliases = myAliases;
   };
+  environment.variables = {
+    # XDG Base Directory specification
+    # See:
+    #  - https://wiki.archlinux.org/title/XDG_Base_Directory#Support
+    #  - https://github.com/b3nj5m1n/xdg-ninja
+    XDG_DATA_HOME = "$HOME/.local/share";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_STATE_HOME = "$HOME/.local/state";
+    XDG_CACHE_HOME = "$HOME/.cache";
+
+    CARGO_HOME = "$XDG_DATA_HOME/cargo";
+    HISTFILE = "$XDG_STATE_HOME/bash/history";
+    NPM_CONFIG_CACHE = "$XDG_CACHE_HOME/npm";
+    NPM_CONFIG_INIT_MODULE = "$XDG_CONFIG_HOME/npm/config/npm-init.js";
+    NPM_CONFIG_TMP = "$XDG_RUNTIME_DIR/npm";
+    RUSTUP_HOME = "$XDG_DATA_HOME/rustup";
+  };
 }
