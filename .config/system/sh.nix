ab575317b7bbea62513da811c33e6b9c4d1bd8ed
Pieter Gerets <pieter.gerets@gmail.com>
2025-04-27 20:50:30 +0200

    Fix assignment of environment variables

--- .config/system/sh.nix
+++ .config/system/sh.nix
@@ -11,6 +11,12 @@ let
     "nix-unfree-develop" = "export NIXPKGS_ALLOW_UNFREE=1 && nix develop --impure";
     "nix-unfree-shell" = "export NIXPKGS_ALLOW_UNFREE=1 && nix shell --impure";
   };
+  baseEnvVariables = {
+    XDG_DATA_HOME = "$HOME/.local/share";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_STATE_HOME = "$HOME/.local/state";
+    XDG_CACHE_HOME = "$HOME/.cache";
+  };
 in
 {
   users.users.pi.shell = pkgs.bash;
@@ -22,16 +28,11 @@ in
     # See:
     #  - https://wiki.archlinux.org/title/XDG_Base_Directory#Support
     #  - https://github.com/b3nj5m1n/xdg-ninja
-    XDG_DATA_HOME = "$HOME/.local/share";
-    XDG_CONFIG_HOME = "$HOME/.config";
-    XDG_STATE_HOME = "$HOME/.local/state";
-    XDG_CACHE_HOME = "$HOME/.cache";
-
-    CARGO_HOME = "$XDG_DATA_HOME/cargo";
-    HISTFILE = "$XDG_STATE_HOME/bash/history";
-    NPM_CONFIG_CACHE = "$XDG_CACHE_HOME/npm";
-    NPM_CONFIG_INIT_MODULE = "$XDG_CONFIG_HOME/npm/config/npm-init.js";
-    NPM_CONFIG_TMP = "$XDG_RUNTIME_DIR/npm";
-    RUSTUP_HOME = "$XDG_DATA_HOME/rustup";
+    CARGO_HOME = baseEnvVariables.XDG_DATA_HOME + "/cargo";
+    HISTFILE = baseEnvVariables.XDG_STATE_HOME + "/bash/history";
+    NPM_CONFIG_CACHE = baseEnvVariables.XDG_CACHE_HOME + "/npm";
+    NPM_CONFIG_INIT_MODULE = baseEnvVariables.XDG_CONFIG_HOME + "/npm/config/npm-init.js";
+    NPM_CONFIG_TMP = "$\{XDG_RUNTIME_DIR}/npm";
+    RUSTUP_HOME = baseEnvVariables.XDG_DATA_HOME + "/rustup";
   };
 }
