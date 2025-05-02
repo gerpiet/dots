fd61f5e5fa77bb075f91ac99af32a329b9a0239a
Pieter Gerets <pieter.gerets@gmail.com>
2025-05-02 20:23:21 +0200

    Add a folder to the Syncthing configuration

--- .config/system/syncthing.nix
+++ .config/system/syncthing.nix
@@ -9,27 +9,31 @@
   services.syncthing = {
     enable = true;
     user = "pi";
-    dataDir = "/home/pi/Documents";
+    dataDir = "home/pi/Documents/syncthing";
     configDir = "/home/pi/.config/syncthing";
     overrideFolders = true;
     overrideDevices = true;
     settings = {
       devices."FP3".id = "RIS5ISR-MNPWDSD-HDJCKKM-B6N5PHA-MFM3DF5-DHO7E2T-OMLUK65-4DME3AT";
       folders = {
-        "obsidian-personal-vault" = {
-          path = "/home/pi/Documents/syncthing/obsidian-personal-vault";
-          devices = [ "FP3" ];
-        };
-        "life-photos" = {
-          path = "/home/pi/Documents/syncthing/life-photos";
-          devices = [ "FP3" ];
-        };
         "app-backup" = {
-          path = "/home/pi/Documents/syncthing/app-backup";
+          path = "~/Documents/syncthing/app-backup";
           devices = [ "FP3" ];
         };
         "keepass" = {
-          path = "/home/pi/Documents/syncthing/keepass";
+          path = "~/Documents/syncthing/keepass";
+          devices = [ "FP3" ];
+        };
+        "life-photos" = {
+          path = "~/Documents/syncthing/life-photos";
+          devices = [ "FP3" ];
+        };
+        "obsidian-personal-vault" = {
+          path = "~/Documents/syncthing/obsidian-personal-vault";
+          devices = [ "FP3" ];
+        };
+        "organize" = {
+          path = "~/Documents/syncthing/organize";
           devices = [ "FP3" ];
         };
       };
